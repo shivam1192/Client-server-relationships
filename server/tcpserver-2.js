c46711911663eff0dac8730dde84a4b1af156005
shivam1192 <sg9827252555@gmail.com>
2019-12-19 23:18:54 +0530

    TCP server for 2 client made

---
+++ server/tcpserver-2.js
@@ -0,0 +1,26 @@
+var net = require("net");
+var sockets = []
+var sum = 0
+var tcpServer = net.createServer((socket)=>{
+       sum=sum+1;
+    console.log("connection is build by client "+ sum)
+    sockets.push(socket)
+    socket.on("data",(data)=>{
+        if(socket[1]!=null){
+              if(sockets[0]==socket){
+                sockets[1].write(data)
+              }
+              else{
+                sockets[0].write(data)
+              }}
+        else{
+            sockets[0].write()
+        }
+    })
+});
+
+tcpServer.maxConnections = 2;
+
+tcpServer.listen("3000",()=>{
+    console.log("server is created")
+});
